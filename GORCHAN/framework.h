1f0441f893d4ad0ea388ad27cc00cec668f7ad12
mshunko <mihail.shunko@yandex.ru>
2023-02-06 21:36:14 +0300

    Настроил отладку дома

--- GORCHAN/framework.h
+++ GORCHAN/framework.h
@@ -3,8 +3,8 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 // Windows Header Files
- 
-#include <windows.h>
+
+//#include <windows.h>
 
 
 #include <locale>
